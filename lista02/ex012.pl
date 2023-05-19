2ae13427c4cf664e63bfa93d3e3a046ddd654148
Matheus Victor <matheusvictor.salles@gmail.com>
2023-05-18 23:16:42 -0300

    Add resolução da questão 12

---
+++ lista02/ex012.pl
@@ -0,0 +1,36 @@
+%fatos
+animal(urso).
+animal(peixe).
+animal(peixinho).
+animal(lince).
+animal(raposa).
+animal(coelho).
+animal(veado).
+animal(guaxinim).
+
+planta(alga).
+planta(grama).
+
+come(urso, peixe).
+come(lince, veado).
+come(urso, raposa).
+come(urso, veado).
+come(peixe, peixinho).
+come(peixinho, alga).
+come(guaxinim, peixe).
+come(raposa, coelho).
+come(coelho, grama).
+come(veado, grama).
+come(urso, guaxinim).
+
+eh_predador(X, Y) :-
+    come(X, Y).
+
+eh_presa(X, Y) :-
+    come(Y, X).
+
+cadeia_alimentar(X) :-
+    (eh_predador(X, A) -> 
+    	write(X), write(' --> '), writeln(A);
+    	write(X), writeln(' não tem presa.'), fail),
+    cadeia_alimentar(A).
